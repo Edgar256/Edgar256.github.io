e2315bfec6ab569e46e1ca47154fcb4874e63614
Edgar <edgar@getpoints.com>
2019-01-03 11:33:05 +0300

    added clock timer

---
+++ js/timer.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+
+	setInterval(function(){
+		var date = new Date();
+		var hrTime = date.getHours();
+		var minTime = date.getMinutes();
+		var secTime= date.getSeconds();
+
+		var minTime0 = minTime.toString();
+		var secTime0 = secTime.toString();
+
+		if (minTime0.length < 2){
+			minTime = '0' + minTime;
+		};
+
+		if (secTime0.length < 2){
+			secTime = '0' + secTime;
+		};
+
+		document.getElementById("hrClock").innerHTML = hrTime;
+		document.getElementById("minClock").innerHTML = minTime;
+		document.getElementById("secClock").innerHTML = secTime;
+
+		if (hrTime < 12){
+			document.getElementById("greet").innerHTML = "Good morning there, have you had breakfast yet?";
+		}
+		if (hrTime > 11 && hrTime < 20){
+			document.getElementById("greet").innerHTML = "Good afternoon there, have you had lunch yet?";
+		}
+		if (hrTime > 19){
+			document.getElementById("greet").innerHTML = "Good evening there,";
+		}
+		
+	}, 1000);
+
+
+});
