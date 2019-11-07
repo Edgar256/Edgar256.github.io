422b9de0d90d5b042ea678036d6020e0296147b1
EDGART <edgar@getpoints.com>
2019-11-07 14:12:39 +0300

    added the yelp project

---
+++ precache-manifest.528e67f94e2bca03aba6a83d0308871e.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33203e0422561d8663450a5b66e09c08",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f3cc9f51d3eb1f9b6960",
+    "url": "./static/css/main.f8945f46.chunk.css"
+  },
+  {
+    "revision": "d715bbb8e6f4094f5d66",
+    "url": "./static/js/2.15de30d4.chunk.js"
+  },
+  {
+    "revision": "f3cc9f51d3eb1f9b6960",
+    "url": "./static/js/main.10d37682.chunk.js"
+  },
+  {
+    "revision": "70cb9bb506652d1dcb84",
+    "url": "./static/js/runtime~main.17dd3a2f.js"
+  },
+  {
+    "revision": "7532850fea7ea581792c1e3543d830c7",
+    "url": "./static/media/anais.7532850f.png"
+  },
+  {
+    "revision": "bbe9d36dca7677b1f6dd7383318ee389",
+    "url": "./static/media/array.bbe9d36d.jpg"
+  },
+  {
+    "revision": "c6df21f8116299a744ccf26f9cee296f",
+    "url": "./static/media/auto-slider.c6df21f8.JPG"
+  },
+  {
+    "revision": "e111cc9dea81505ac69d3825a30f6dbc",
+    "url": "./static/media/binary.e111cc9d.jpg"
+  },
+  {
+    "revision": "5fb0101d7ab10004a224ad766b496f76",
+    "url": "./static/media/bmi.5fb0101d.jpg"
+  },
+  {
+    "revision": "d7272eb4bdc95568662b38e59328cc63",
+    "url": "./static/media/clock.d7272eb4.jpg"
+  },
+  {
+    "revision": "99b5210f771dc80fa53c304b4f8b1ad1",
+    "url": "./static/media/ed.99b5210f.png"
+  },
+  {
+    "revision": "2a6edce2988a1e7cddc1bb07c213e591",
+    "url": "./static/media/edgar.2a6edce2.jpg"
+  },
+  {
+    "revision": "3f32213ad6870a4ba17a5c44bd6b50f1",
+    "url": "./static/media/equation.3f32213a.jpg"
+  },
+  {
+    "revision": "efe5fe0a4e00d3613ed1454754f3943d",
+    "url": "./static/media/excel.efe5fe0a.png"
+  },
+  {
+    "revision": "6fc05601e59f64be7b1de9cffd71c29a",
+    "url": "./static/media/favelist.6fc05601.png"
+  },
+  {
+    "revision": "f2da9f85035ddb921eb72fd84ebecbb6",
+    "url": "./static/media/freeWeb.f2da9f85.png"
+  },
+  {
+    "revision": "5339e37d20c40cff26cfd7ce28f72b8b",
+    "url": "./static/media/frog.5339e37d.jpg"
+  },
+  {
+    "revision": "c97710f99249650a7e00fcde7d5f8000",
+    "url": "./static/media/front-wall-paper-1.c97710f9.jpg"
+  },
+  {
+    "revision": "b71c1d18a8d77d372ec323d364f1b74f",
+    "url": "./static/media/geek-label.b71c1d18.png"
+  },
+  {
+    "revision": "88393a5a5782eb76c3801b2e2b669b79",
+    "url": "./static/media/getpoints.88393a5a.png"
+  },
+  {
+    "revision": "ea66bddd1e6f7034f216b4aeeca929bd",
+    "url": "./static/media/image-slider.ea66bddd.png"
+  },
+  {
+    "revision": "e29ffffd6f3c8f812a8d922c301b21d4",
+    "url": "./static/media/iphone.e29ffffd.jpg"
+  },
+  {
+    "revision": "be16947b472fbc19f768784d63f67b00",
+    "url": "./static/media/jQuery-logo.be16947b.jpg"
+  },
+  {
+    "revision": "03cdcbedc250f87ef98ee64370a63cb0",
+    "url": "./static/media/jquery-div.03cdcbed.png"
+  },
+  {
+    "revision": "f9c3c5eca47f7b133e1007a3ed902dde",
+    "url": "./static/media/js-code.f9c3c5ec.png"
+  },
+  {
+    "revision": "f3fd4ab348716410b97b5865ac80a419",
+    "url": "./static/media/lights.f3fd4ab3.jpg"
+  },
+  {
+    "revision": "7d772656028b8d23487822a45f8972a7",
+    "url": "./static/media/login.7d772656.JPG"
+  },
+  {
+    "revision": "1ab37cd349c118fa9bc00633ffb26f19",
+    "url": "./static/media/pop-up.1ab37cd3.jpg"
+  },
+  {
+    "revision": "ca31f74a20da556b69057d20ff6abaf2",
+    "url": "./static/media/react-js.ca31f74a.png"
+  },
+  {
+    "revision": "87cc25f5bfc417ff9887e384f5a8be5b",
+    "url": "./static/media/search.87cc25f5.jpg"
+  },
+  {
+    "revision": "ed5358dfe081c6150259811b31d03ebd",
+    "url": "./static/media/stat-img.ed5358df.png"
+  },
+  {
+    "revision": "a1010c16852b8bc81fc8066f613ae8bb",
+    "url": "./static/media/time-counter.a1010c16.jpg"
+  },
+  {
+    "revision": "58526b317fc863a2085bc5c6dc6ca203",
+    "url": "./static/media/yelp.58526b31.png"
+  }
+]);
--- projects/yelp/service-worker.js
+++ projects/yelp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.528e67f94e2bca03aba6a83d0308871e.js"
+  "/precache-manifest.79d17717098a8ea9beba6a06a5e1943a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
