d72f0b9ae8517df83d30ebe2855f33efe480b224
EDGART <edgar@getpoints.com>
2020-05-06 15:06:21 +0300

    opacity fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e2199fb7503c1cac8f73d3d50a6e1f0f.js"
+  "./precache-manifest.2012ecc3d37d3224993298d8de093fab.js"
 );
 
 self.addEventListener('message', (event) => {
