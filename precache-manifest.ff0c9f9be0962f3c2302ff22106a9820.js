a0e9a050913c8b0c9e590d0a3e642d7475080855
edgar256m <tinka.edgar@gmail.com>
2021-03-11 15:29:30 +0300

    new profile

---
+++ precache-manifest.ff0c9f9be0962f3c2302ff22106a9820.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1faf4652b8f4573fbf0c8225c546a687",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21a54dfb5e780863675b",
+    "url": "./static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "fab805da265d592f7aa5",
+    "url": "./static/css/main.eae8e44c.chunk.css"
+  },
+  {
+    "revision": "21a54dfb5e780863675b",
+    "url": "./static/js/2.76a13e2d.chunk.js"
+  },
+  {
+    "revision": "fab805da265d592f7aa5",
+    "url": "./static/js/main.7a1bd8e2.chunk.js"
+  },
+  {
+    "revision": "70cb9bb506652d1dcb84",
+    "url": "./static/js/runtime~main.17dd3a2f.js"
+  },
+  {
+    "revision": "2a6edce2988a1e7cddc1bb07c213e591",
+    "url": "./static/media/edgar.2a6edce2.jpg"
+  },
+  {
+    "revision": "ced9d60c77bd2e858bcab129e429484a",
+    "url": "./static/media/pen-4337521_1280.ced9d60c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2012ecc3d37d3224993298d8de093fab.js"
+  "./precache-manifest.ff0c9f9be0962f3c2302ff22106a9820.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
