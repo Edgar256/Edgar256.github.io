f820f274ac1f1098a4174f71b6e51db2ead81489
edgar256 <tinka.edgar@gmail.com>
2021-12-31 21:55:37 +0300

    Updates

---
+++ precache-manifest.2316d146c76f3eb834cfc861ce92b6d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd19e37e7332fae2fcf50ce2cc247f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b48dff138721a9244037",
+    "url": "/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "7f59bf262c8a587fba87",
+    "url": "/static/css/main.63e6d5a4.chunk.css"
+  },
+  {
+    "revision": "b48dff138721a9244037",
+    "url": "/static/js/2.11a44a33.chunk.js"
+  },
+  {
+    "revision": "7f59bf262c8a587fba87",
+    "url": "/static/js/main.126eac02.chunk.js"
+  },
+  {
+    "revision": "73db4d9d43a5ce300db9",
+    "url": "/static/js/runtime~main.c0b819b0.js"
+  },
+  {
+    "revision": "ced9d60c77bd2e858bcab129e429484a",
+    "url": "/static/media/pen-4337521_1280.ced9d60c.jpg"
+  }
+]);
