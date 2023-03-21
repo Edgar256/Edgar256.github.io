063a8e3b529ca293458da6fbc66800ef051c4cd9
edgar256 <tinka.edgar@gmail.com>
2023-03-21 05:37:41 +0300

    Added socials

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import ParticlesBackground from './components/ParticlesBackground';
 import Typewriter from 'typewriter-effect';
+
+// image imports
 import Edgar from './images/edgar-photo.jpg';
 
 function App() {
   return (
     <div>
       <ParticlesBackground />
-      <div className="container position-relative vh-100">
+      <div className="container position-relative">
         <div className="d-flex justify-content-center pt-3">
-          <nav className="nav text-white">
+          <nav className="nav text-white fs-4">
             <a className="nav-link text-white" href="#">
               Home
             </a>
@@ -31,20 +33,22 @@ function App() {
             </a>
           </nav>
         </div>
-        <div className="w-100 h-100 d-flex justify-content-center align-items-center p-0">
+      </div>
+      <div className="container position-relative min-vh-100 d-flex justify-content-center align-items-center">
+        <div className="w-100 h-100  p-0 py-5">
           <div className="d-flex w-100">
             <img
               src={Edgar}
               alt="Edgar profile"
               className="rounded-circle mx-auto py-1 px-2"
-              width={200}
-              height={200}
+              width={150}
+              height={150}
             />
-            <div className="w-100 px-2 py-0">
-              <div className="text-white display-1">
+            <div className="w-100 px-2 py-2">
+              <div className="text-white display-2">
                 Hi, I'm Edgar Tinkamanyire,
               </div>
-              <div className="text-white display-4">
+              <div className="text-white fs-4">
                 <Typewriter
                   options={{
                     strings: [
@@ -61,11 +65,11 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="container position-relative vh-100">
+      <div className="container position-relative min-vh-100">
         <div className="w-100 h-100 d-flex justify-content-center align-items-center p-0">
           <div>
-            <div className="text-white display-4 py-3 text-center">About</div>
-            <div className="text-white text-center">
+            <div className="text-white display-4 py-5 text-center">About</div>
+            <div className="text-white text-center py-5">
               I'm a FrontEnd Developer at Mobile Paradigm Group (MPG) a Tech
               Company in Uganda(East-Africa). I have a big passion for UI
               effects, dynamic user experience, website and mobile-app
@@ -80,9 +84,9 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="container position-relative vh-100">
-        <div className="w-100 h-100 d-flex justify-content-center_ align-items-center p-0">
-          <div className='w-100'>
+      <div className="container position-relative min-vh-100 py-5 my-5">
+        <div className="w-100 h-100 d-flex align-items-center p-0">
+          <div className="w-100">
             <div className="text-white display-4 py-3 text-center py-5 w-100">
               Technologies
             </div>
@@ -231,7 +235,7 @@ function App() {
               <div className="d-flex flex-wrap">
                 <button className="btn btn-outline-secondary text-white rounded-pill m-1">
                   AWS
-                </button>                
+                </button>
                 <button className="btn btn-outline-secondary text-white rounded-pill m-1">
                   HEROKU
                 </button>
@@ -240,11 +244,118 @@ function App() {
                 </button>
                 <button className="btn btn-outline-secondary text-white rounded-pill m-1">
                   LINODE
-                </button>    
+                </button>
                 <button className="btn btn-outline-secondary text-white rounded-pill m-1">
                   DIGITAL OCEAN
-                </button>           
+                </button>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container position-relative min-vh-100 my-5 py-5">
+        <div className="w-100 h-100 d-flex align-items-center p-0">
+          <div className="w-100">
+            <div className="text-white display-4 py-5 text-center">
+              Skills & Resume
+            </div>
+            <div className="text-white py-3">
+              Download my full PDF resume here
+            </div>
+            <div className="d-flex text-white w-100 p-3">
+              <h1 className="bi-list-nested display-1 px-2"></h1>
+              <div className="py-1 px-3">
+                <h2>Agile Development</h2>
+                <p>
+                  I have over 5 years experience working in Agile development
+                </p>
+              </div>
+            </div>
+            <div className="d-flex text-white w-100 p-3">
+              <h1 className="bi-person-bounding-box display-1 px-2"></h1>
+              <div className="py-1 px-3">
+                <h2>Leadership Skills & Team work</h2>
+                <p>
+                  For the last 5 years , I have hard an opportunity to lead a
+                  team of my own. I have a lot of experience working with other
+                  people in a team both in-house and remotely.
+                </p>
+              </div>
+            </div>
+            <div className="d-flex text-white w-100 p-3">
+              <h1 className="bi-people-fill display-1 px-2"></h1>
+              <div className="py-1 px-3">
+                <h2>Collaboration Skills</h2>
+                <p>
+                  I boast off a vast experience in major collaboration tools
+                  like Slack for communication, Invision & Figma for prototyping
+                  and wireframing, Postman & Swagger for API documentation, Git
+                  for version control.
+                </p>
+              </div>
+            </div>
+            <div className="d-flex text-white w-100 p-3">
+              <h1 className="bi-x-diamond-fill display-1 px-2"></h1>
+              <div className="py-1 px-3">
+                <h2>Pixel Perfect</h2>
+                <p>
+                  My designs are pixel perfect, with strong emphasis on user
+                  empathy
+                </p>
+              </div>
+            </div>
+            <div className="d-flex text-white w-100 p-3">
+              <h1 className="bi-columns-gap display-1 px-2"></h1>
+              <div className="py-1 px-3">
+                <h2>Responsive Design</h2>
+                <p>
+                  All the website apps I build are fully mobile responsive
+                  across all devices.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container position-relative min-vh-100 my-5 py-5 d-flex justify-content-center align-items-center">
+        <div className="w-100">
+          <div className="text-white display-4 py-5 text-center py-5">
+            Contact
+          </div>
+          <div className="">
+            <div className="card bg-transparent">
+              <ul className="list-group list-group-flush bg-transparent">
+                <li className="list-group-item bg-transparent text-white row">
+                  <span className="col-4 col-sm-6">
+                    Email <i className="bi-envelope-fill px-2"></i> :
+                  </span>
+                  <span className="col-sm-8 text-break">tinka.edgar@gmail.com</span>
+                </li>
+                <li className="list-group-item bg-transparent text-white row">
+                  <span className="col-sm-4">
+                    Stackoverflow <i className="bi-stack-overflow px-2"></i> :
+                  </span>
+                  <span className="col-sm-8 text-break">https://stackoverflow.com/users/6561907/edgar256</span>
+                </li>
+                <li className="list-group-item bg-transparent text-white row">
+                  <span className="col-sm-4">
+                    Linkedin <i className="bi-linkedin px-2"></i> :
+                  </span>
+                  <span className="col-sm-8 text-break">https://www.linkedin.com/in/tinkamanyire-edgar/</span>
+                </li>
+                <li className="list-group-item bg-transparent text-white row">
+                  <span className="col-sm-4">
+                    Github <i className="bi-github px-2"></i> :
+                  </span>
+                  <span className="col-sm-8 text-break">https://github.com/Edgar256</span>
+                </li>
+                <li className="list-group-item bg-transparent text-white row">
+                  <span className="col-sm-4">
+                    Codewars <i className="bi-code-slash px-2"></i> :
+                  </span>
+                  <span className="col-sm-8 text-break">https://www.codewars.com/users/Edgar256</span>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
