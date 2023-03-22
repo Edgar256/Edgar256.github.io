56f1cd69b0e5728d85b430baacdd1ebc8e008731
edgar256 <tinka.edgar@gmail.com>
2023-03-22 06:53:24 +0300

    Changed from BrowserRouter to HashRouter to fix refresh issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Error from './pages/Error';
 import Landing from './pages/Landing';
 import Project from './pages/Project';
