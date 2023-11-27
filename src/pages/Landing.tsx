28eb797a285da78ea711b48a61b2877f41179809
edgar256 <tinka.edgar@gmail.com>
2023-11-27 15:55:00 +0300

    ft - responsive fixes

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -798,7 +798,7 @@ function Landing() {
             <div className="" id="contact">
               <div className="card bg-transparent">
                 <ul className="list-group list-group-flush bg-transparent fs-4">
-                  <li className="list-group-item bg-transparent row text-lg-start text-center py-3">
+                  <li className="list-group-item bg-transparent text-lg-start text-center py-3">
                     <span className="col-4 col-sm-6">
                       Email
                       <i className="bi-envelope-fill px-2 text-warning"></i>:
@@ -815,7 +815,7 @@ function Landing() {
                       </Link>
                     </span>
                   </li>
-                  <li className="list-group-item bg-transparent row text-lg-start text-center py-3">
+                  <li className="list-group-item bg-transparent text-lg-start text-center py-3">
                     <span className="col-sm-4">
                       Stackoverflow
                       <i className="bi-stack-overflow px-2 text-danger"></i>:
@@ -831,13 +831,13 @@ function Landing() {
                       </Link>
                     </span>
                   </li>
-                  <li className="list-group-item bg-transparent row text-lg-start text-center py-3">
+                  <li className="list-group-item bg-transparent text-lg-start text-center py-3">
                     <span className="col-sm-4">
                       Linkedin<i className="bi-linkedin px-2 text-info"></i>:
                     </span>{" "}
                     <span>
                     <Link
-                      className="col-sm-8 text-break  text-decoration-none text-muted"
+                      className="col-sm-8 text-break text-decoration-none text-muted"
                       to="https://www.linkedin.com/in/tinkamanyire-edgar/"
                       target="_blank"
                       rel="noopener noreferrer"
@@ -846,7 +846,7 @@ function Landing() {
                     </Link>
                     </span>
                   </li>
-                  <li className="list-group-item bg-transparent row text-lg-start text-center py-3">
+                  <li className="list-group-item bg-transparent text-lg-start text-center py-3">
                     <span className="col-sm-4">
                       Github<i className="bi-github px-2"></i>:
                     </span>{" "}
@@ -861,7 +861,7 @@ function Landing() {
                     </Link>
                     </span>
                   </li>
-                  <li className="list-group-item bg-transparent row text-lg-start text-center py-3">
+                  <li className="list-group-item bg-transparent text-lg-start text-center py-3">
                     <span className="col-sm-4">
                       Codewars<i className="bi-code-slash px-2"></i>:
                     </span>{" "}
